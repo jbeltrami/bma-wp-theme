f36af5b1fe12d7a32a3892b491cb6c8deaa5798e
jbeltrami <joaopbeltrami@gmail.com>
2019-12-23 09:58:10 -0500

    Create dynamic Menu

--- page.php
+++ page.php
@@ -25,4 +25,5 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+$context['menu'] = new Timber\Menu( 'main-menu' );
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
